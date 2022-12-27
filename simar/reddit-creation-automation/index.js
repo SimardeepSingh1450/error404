d1ff255b50830e8f9321706d2aa089f6cff6f35b
simardeepsm <smudhar_be21@thapar.edu>
2022-12-27 23:48:13 +0530

    social-media-credentials

--- simar/reddit-creation-automation/index.js
+++ simar/reddit-creation-automation/index.js
@@ -1,8 +1,10 @@
 const fakePerson=require('../SockPuppet/fakeperson.json');
+const fakeGmail=require('../../FakeGmail.json');
 const puppeteer=require('puppeteer');
+const puppeteerExtra=require('puppeteer-extra');
+const StealthPlugin=require('puppeteer-extra-plugin-stealth');
 
 //Plugins to solve captcha
-// const StealthPlugin=require('puppeteer-extra-plugin-stealth');
 // const {executablePath}=require('puppeteer');
 
 // //dotenv
@@ -17,6 +19,8 @@ async function run(){
 // const pathToExtension=require('path').join(__dirname,'2captcha-solver');
 // puppeteer.use(StealthPlugin());
 
+puppeteerExtra.use(StealthPlugin());
+
 const browser=await puppeteer.launch({
     headless:false,
     defaultViewport:false
@@ -29,35 +33,53 @@ const browser=await puppeteer.launch({
 
 // const [page]=await browser.pages();
 const page=await browser.newPage();
+const redditLoginUrl="https://www.reddit.com/login/?dest=https%3A%2F%2Fwww.reddit.com%2F";
+const gmailLoginUrl="https://accounts.google.com/v3/signin/identifier?dsh=S369608870%3A1672153336878500&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=AeAAQh4GBVwKgqUpOCX42cgD6XAP_oMZ1i_n87WkH2impMPjs9RcaP1yFff1LSdoTVWifnEq6vhvbw";
 
-await page.goto("https://www.reddit.com/register/",{waitUntil:'load',timeout:0});
+//Gmail Login First
+await page.goto(gmailLoginUrl,{waitUntil:'load',timeout:0});
 
-//selecting the email-Input Element
-const emailInput=await page.waitForSelector("#regEmail");
-await emailInput.focus();
-await page.keyboard.type(fakePerson.Email+"\n");
+await page.waitForTimeout(2000);
+await page.keyboard.type(fakeGmail.email+"\n");
 
-await page.waitForTimeout(4000);
+await page.waitForTimeout(3000);
+await page.keyboard.type(fakeGmail.pass+"\n");
 
-//await for the next page USERNAME and PASSWORD Input
-// const userNameInput=await page.waitForSelector("#regUsername");
-// await userNameInput.focus();
-// await page.keyboard.type(fakePerson.Username);
+await page.waitForTimeout(3000);
 
-// await page.waitForTimeout(4000);
+//Reddit Login Second
+await page.goto(redditLoginUrl,{waitUntil:'load',timeout:0});
+await page.waitForTimeout(2000);
 
-// const passwordInput=await page.waitForSelector("#regPassword");
-// await passwordInput.focus();
-// await page.keyboard.type(fakePerson.Password);
 
-// await page.waitForTimeout(4000);
+// await page.goto("https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?redirect_uri=storagerelay%3A%2F%2Fhttps%2Fwww.reddit.com%3Fid%3Dauth936064&response_type=permission%20id_token&scope=email%20profile%20openid&openid.realm&include_granted_scopes=true&prompt=select_account&client_id=705819728788-b2c1kcs7tst3b7ghv7at0hkqmtc68ckl.apps.googleusercontent.com&ss_domain=https%3A%2F%2Fwww.reddit.com&fetch_basic_profile=true&gsiwebsdk=2&service=lso&o2v=1&flowName=GeneralOAuthFlow")
+// await page.waitForTimeout(2000);
+await page.click("#google-sso");
 
-// await page.waitForSelector('.recaptcha-checkbox-border')
-// await page.click('.recaptcha-checkbox-border');
+await page.waitForTimeout(2000);
 
-// await page.waitForSelector(`.recaptcha-checkbox-border[data-state="solved"]`,{timeout:18000})
+await page.click(".tgnCOd");
 
-// await page.click("button[type='submit']");
+await page.waitForTimeout(2000);
+
+// await page.goto("http://reddit.com");
+
+
+// await page.keyboard.type(fakeGmail.email+"\n");
+
+
+
+
+// await page.waitForTimeout(1000);
+
+
+// await page.waitForTimeout(3000);
+// //entering gmail credentials
+// // const emailInput=await page.evaluate(()=>document.querySelector("#identifierId.whsOnd.zHQkBf"));
+// // await emailInput.focus();
+// await page.keyboard.type(fakeGmail.email+"\n");
+
+
 
 //await browser.close();
 
