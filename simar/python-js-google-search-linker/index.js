72928edc9588d5d7102f0ea20727e9774adc138e
simardeepsm <smudhar_be21@thapar.edu>
2022-12-28 17:21:55 +0530

    some-changes

---
+++ simar/python-js-google-search-linker/index.js
@@ -0,0 +1,7 @@
+// const express=require('express');
+// const app=express();
+
+//python code runner module-> child_process
+const {spawn}=require('child_process');
+
+const chlidPython=spawn('python',[''])
