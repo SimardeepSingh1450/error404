f1d84e494c0e7e865427de9e65e96f65dd751b66
Kulwinder <laddiaulakh2002@gmail.com>
2022-12-29 23:12:20 +0530

    sockpuppet frontend prototype

--- simar/website/client/src/components/Main.js
+++ simar/website/client/src/components/Main.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import '../css/Main.css'
-import Maps from './Maps'
+import React from "react";
+import "../css/Main.css";
+import Maps from "./Maps";
+import Sockpuppet from "./Sockpuppet";
 
 const Main = () => {
   return (
-    <div className='container'>
-        <section className='one'>
+    <div className="container">
+      <section className="one">
         <h1>First Page</h1>
-        </section>
-        <section className='two'>
+        <Sockpuppet />
+      </section>
+      <section className="two">
         <h1>Google Maps</h1>
-        <Maps/>
-        </section>
-        <section className='third'>
+        <Maps />
+      </section>
+      <section className="third">
         <h1>Third Page</h1>
-        </section>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
---
+++ simar/website/client/src/components/Sockpuppet.js
@@ -0,0 +1,127 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
+const Sockpuppet = () => {
+  const [sockpuppetdata, setsockpuppetdata] = useState();
+  const getsockpuppetdata = async () => {
+    const sockpuppet_data = await axios.get("http://localhost:3001/sockpuppet");
+    console.log(sockpuppet_data.data.data);
+    setsockpuppetdata(sockpuppet_data.data.data);
+  };
+
+  useEffect(() => {
+    getsockpuppetdata();
+  }, []);
+
+  return (
+    <div className="App">
+      {sockpuppetdata && (
+        <div>
+          <li>
+            <label for="about">Name:</label>
+            <span id="about">{sockpuppetdata.Name}</span>
+          </li>
+          <li>
+            <label for="about">First Name</label>
+            <span id="about">{sockpuppetdata.firstName}</span>
+          </li>
+          <li>
+            <label for="about">Last Name:</label>
+            <span id="about">{sockpuppetdata.lastName}</span>
+          </li>
+          <li>
+            <label for="about">Address:</label>
+            <span id="about">{sockpuppetdata.Address}</span>
+          </li>
+          <li>
+            <label for="about">Age:</label>
+            <span id="about">{sockpuppetdata.Age}</span>
+          </li>
+          <li>
+            <label for="about">User Name:</label>
+            <span id="about">{sockpuppetdata.Username}</span>
+          </li>
+          <li>
+            <label for="about">Email:</label>
+            <span id="about">{sockpuppetdata.Email}</span>
+          </li>
+          <li>
+            <label for="about">Password</label>
+            <span id="about">{sockpuppetdata.Password}</span>
+          </li>
+          <li>
+            <label for="about">Gmail:</label>
+            <span id="about">{sockpuppetdata.gmail}</span>
+          </li>
+          <li>
+            <label for="about">Gmail Pass:</label>
+            <span id="about">{sockpuppetdata.gmailPass}</span>
+          </li>
+          <li>
+            <label for="about">Phone Number</label>
+            <span id="about">{sockpuppetdata.PhoneNumber}</span>
+          </li>
+          <li>
+            <label for="about">Birthday:</label>
+            <span id="about">{sockpuppetdata.Birthday}</span>
+          </li>
+          <li>
+            <label for="about">Blood Type:</label>
+            <span id="about">{sockpuppetdata.BloodType}</span>
+          </li>
+          <li>
+            <label for="about">Browser Agent</label>
+            <span id="about">{sockpuppetdata.BrowserAgent}</span>
+          </li>
+          <li>
+            <label for="about">Debit Card No:</label>
+            <span id="about">{sockpuppetdata.DEBITCARD}</span>
+          </li>
+          <li>
+            <label for="about">CVV Number:</label>
+            <span id="about">{sockpuppetdata.CVV}</span>
+          </li>
+          <li>
+            <label for="about">Card Expiry Date:</label>
+            <span id="about">{sockpuppetdata.cardExpiry}</span>
+          </li>
+          <li>
+            <label for="about">GUID:</label>
+            <span id="about">{sockpuppetdata.GUID}</span>
+          </li>
+          <li>
+            <label for="about">SNN:</label>
+            <span id="about">{sockpuppetdata.SSN}</span>
+          </li>
+          <li>
+            <label for="about">Vehicle:</label>
+            <span id="about">{sockpuppetdata.Vehicle}</span>
+          </li>
+          <li>
+            <label for="about">Weight:</label>
+            <span id="about">{sockpuppetdata.Weight}</span>
+          </li>
+          <li>
+            <label for="about">Height:</label>
+            <span id="about">{sockpuppetdata.Height}</span>
+          </li>
+          <li>
+            <label for="about">Mother Maiden Name:</label>
+            <span id="about">{sockpuppetdata.MotherMaidenName}</span>
+          </li>
+          <li>
+            <label for="about">Zodiac Symbol:</label>
+            <span id="about">{sockpuppetdata.Zodiac}</span>
+          </li>
+          <li>
+            <label for="about">Geo Points:</label>
+            <span id="about">{sockpuppetdata.geoPoints}</span>
+          </li>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Sockpuppet;
